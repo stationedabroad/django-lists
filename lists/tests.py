6cfa2ca4413963c3c3b41c200df00cf136d1df35
sulman shaikh <sulmanmh@gmail.com>
2020-03-20 00:10:45 +0000

    minimal html returned (for testinggit status)

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -10,3 +12,11 @@ class SMokeTest(TestCase):
     def test_root_url_resolves_to_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        content = response.content.decode('utf8')
+        self.assertTrue(content.startswith('<html>'))
+        self.assertIn('<title>To-Do Lists</title>', content)
+        self.assertTrue(content.endswith('</html>'))
